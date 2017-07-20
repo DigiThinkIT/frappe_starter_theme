9c25c9402b895ed7d9bb976f5593960211514a94
Valmik Jangla <valmik2k2@gmail.com>
2017-07-20 18:15:35 +0530

    Added Payment Details on the Account Page

--- frappe_starter_theme/www/me.py
+++ frappe_starter_theme/www/me.py
@@ -11,7 +11,20 @@ no_cache = 1
 no_sitemap = 1
 
 def get_context(context):
+	from awesome_cart.compat.customer import get_current_customer
 	if frappe.session.user=='Guest':
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
 
 	context.show_sidebar=True
+
+	customer = get_current_customer()
+
+	context["total_unpaid"] = frappe.db.sql("""select sum(outstanding_amount)
+	from `tabSales Invoice`
+	where customer=%s and docstatus = 1""", customer.name)[0][0] or ""
+
+	context["credit_limit"] = customer.credit_limit
+	context["credit_days_based_on"] = customer.credit_days_based_on
+	context["credit_days"] = customer.credit_days
+	
+	return context
